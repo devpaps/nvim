af7120f037c0d32aa07643b555b513062a6f6db2
devpaps <crymeat@gmail.com>
2022-03-21 08:44:36 +0100

    fix:disabled nvimtree and packer

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ require('lualine').setup {
     theme = 'gruvbox',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
+    disabled_filetypes = {'packer','NvimTree'},
     always_divide_middle = true,
     globalstatus = false,
   },
