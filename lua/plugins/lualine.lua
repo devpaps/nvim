1206585ba242a2722fe4cd4de64117deea48364d
devpaps <crymeat@gmail.com>
2022-03-24 13:03:16 +0100

    chore: changed colortheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'nordfox',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {'packer','NvimTree'},
