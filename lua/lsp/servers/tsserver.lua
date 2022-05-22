5fc7589d3a1ff5c911df758b228ea8fbf676290f
devpaps <crymeat@gmail.com>
2022-05-22 21:16:43 +0200

    fix: tsserver formatting

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -34,8 +34,8 @@ capabilities.textDocument.codeAction = {
 }
 
 local on_attach = function(client, bufnr)
-  client.server_capabilities.documentFormattingProvider = false
-  client.server_capabilities.documentRangeFormattingProvider = false
+  client.server_capabilities.documentFormattingProvider = true
+  client.server_capabilities.documentRangeFormattingProvider = true
 
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
