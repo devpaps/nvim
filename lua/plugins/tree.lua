c078c36f2f52ed52b56e3d5ede15089b6c58e7fb
devpaps <crymeat@gmail.com>
2022-09-10 09:49:37 +0200

    fix: added icons etc

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,4 +1,18 @@
--- vim.g.nvim_tree_respect_buf_cwd = 1
+-- local utils = require('utils')
+local nvim_tree_events = require('nvim-tree.events')
+local bufferline_state = require('bufferline.state')
+
+local TREE_WIDTH = 40
+
+local git_icons = {
+  unstaged = "",
+  staged = "",
+  unmerged = "",
+  renamed = "➜",
+  untracked = "",
+  deleted = "",
+  ignored = "◌"
+}
 
 local keymappings = {
   { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
@@ -49,22 +63,44 @@ require'nvim-tree'.setup {
   open_on_setup       = false,
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup  = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  -- auto_close          = false,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab         = false,
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor       = false,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
   update_cwd          = true,
+  -- opens in place of the unnamed buffer if it's empty
+  hijack_unnamed_buffer_when_opening = false,
+  --false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
+  respect_buf_cwd = true,
   -- show lsp diagnostics in the signcolumn
   diagnostics         = {
-    enable = true,
+    enable = false,
     icons  = {
       hint    = "",
       info    = "",
       warning = "",
       error   = "",
+    }
+  },
+  renderer = {
+    add_trailing = false,
+    group_empty = true,
+    highlight_git = true,
+    highlight_opened_files = "none",
+    root_folder_modifier = ":~",
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      glyphs = {
+        git = git_icons
+      }
     }
   },
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
@@ -81,27 +117,46 @@ require'nvim-tree'.setup {
   -- configuration options for the system open command (`s` in the tree by default)
   system_open = {
     -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
+    cmd  = "",
     -- the command arguments as a list
     args = {}
   },
   filters = {
     dotfiles = false,
-    custom = {'.git', 'node_modules', '.cache', '.bin'}
+    custom = {}
   },
   git = {
     enable = true,
     ignore = true,
     timeout = 500,
   },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      -- if true the tree will resize itself after opening a file
+      resize_window = true,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
   view = {
     -- width of the window, can be either a number (columns) or a string in `%`
-    width = 40,
+    width = TREE_WIDTH,
     hide_root_folder = false,
     -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'left',
-    -- if true the tree will resize itself after opening a file
-    -- auto_resize = true,
     mappings = {
       -- custom only false will merge the list with the default mappings
       -- if true, it will only use your list to set the mappings
@@ -109,14 +164,8 @@ require'nvim-tree'.setup {
       -- list of mappings to set on the tree manually
       list = keymappings
     },
-    number = true,
-    relativenumber = true
-  },
-    actions = {
-    change_dir = {
-      enable = true,
-      global = true,
-    },
+    number = false,
+    relativenumber = false
   },
   trash = {
     cmd = "trash",
@@ -124,5 +173,12 @@ require'nvim-tree'.setup {
   }
 }
 
+vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>lua require'nvim-tree'.toggle()<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>lua require'nvim-tree'.toggle()<CR>", {noremap = true, silent = true})
+nvim_tree_events.on_tree_open(function ()
+    bufferline_state.set_offset(TREE_WIDTH + 1, 13 .. 'File Explorer')
+end)
+
+nvim_tree_events.on_tree_close(function ()
+    bufferline_state.set_offset(0)
+end)
