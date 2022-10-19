cbd46178a73fcf536aa882666c4d69678ca0dabb
Lars Jönsson <crymeat@gmail.com>
2022-10-19 12:03:07 +0200

    fix: changed to default_capabilites because of breaking changes in cmp_nvim

--- lua/lsp/installer.lua
+++ lua/lsp/installer.lua
@@ -14,7 +14,7 @@ lsp_installer.on_server_ready(function(server)
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
     if status_ok then
-      capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
     end
 
     local opts = {
