6a17d0c404b376ab0eec743007f53bbc0492cb2b
Lars Jönsson <crymeat@gmail.com>
2023-05-26 22:14:08 +0200

    add catppuccin theme

--- lua/config.lua
+++ lua/config.lua
@@ -4,11 +4,11 @@ local icons = require("icons")
 
 M42 = {
   icons = icons,
-  colorscheme = 'nordfox',
+  colorscheme = 'catppuccin-mocha',
   ui = {
     float = {
       border = 'rounded',
-      highlight = 'nordfox' -- check available by :Telescope highlights
+      highlight = 'catppuccin-mocha' -- check available by :Telescope highlights
     },
     background = {
       guifg = "#1D2122"
